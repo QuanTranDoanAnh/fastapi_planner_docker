99eb5ace4b7fcb2f0bf0c8ceb6ea29a3cf48999b
QuanTranDoanAnh <td.anhquan@gmail.com>
2023-05-28 20:52:53 +0900

    Add creator field from authenticated function and CORS

--- routes/events.py
+++ routes/events.py
@@ -32,6 +32,7 @@ async def retrieve_event(id: PydanticObjectId) -> Event:
 
 @event_router.post("/new")
 async def create_event(body: Event = Body(...), user: str=Depends(authenticate)) -> dict:
+    body.creator = user
     await event_database.save(body)
     return {
         "message": "Event created successfully"
@@ -39,12 +40,21 @@ async def create_event(body: Event = Body(...), user: str=Depends(authenticate))
 
 @event_router.put("/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, body: EventUpdate, user: str=Depends(authenticate)) -> Event:
+    event = await event_database.get(id)
+    
+    if event.creator != user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Operation not allowed"
+        )
+    
     updated_event = await event_database.update(id, body)
     if not updated_event:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Event with supplied ID does not exist"
         )
+    
     return updated_event
 
 @event_router.delete("/{id}")
@@ -54,6 +64,11 @@ async def delete_event(id: PydanticObjectId, user: str=Depends(authenticate)) ->
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Event with supplied ID does not exist"
+        )
+    if event.creator != user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Operation not allowed"
         )
     return {
         "message": "Event deleted successfully"
